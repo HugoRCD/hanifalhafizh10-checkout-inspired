506e6a4e43b6955134d7def465fd4d1c89e9cc34
HugoRCD <hugo.richard@epitech.eu>
2024-01-16 16:52:05 +0100

    Update color and add modal transition

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,101 +1,135 @@
 <script setup lang="ts">
-const openModal = ref(false);
+const openModal = ref(true);
 </script>
 
 <template>
-    <Button label="Open modal" @click="openModal = true" />
-    <div class="container rounded-xl" v-show="openModal">
-        <div class="section flex justify-between items-center">
-            <Button label="Cancel" circle />
-            <h2 class="text-muted text-lg">
-                Send payment
-            </h2>
-            <Button label="Close" circle @click="openModal = false" />
-        </div>
-        <div class="divider" />
-        <div class="section flex justify-between items-center">
-            <div class="flex items-center gap-4">
-                <img src="https://avatars.githubusercontent.com/u/71938701?v=4" alt="avatar"
-                    class="size-14 rounded-full border-2 border-black" />
-                <div>
-                    <h4>
-                        Hugo Richard
-                    </h4>
-                    <p class="text-muted">
-                        contact@gmail.com
-                    </p>
-                </div>
-            </div>
-            <Button label="+" circle />
-        </div>
-        <div class="divider" />
-        <div class="section flex flex-col gap-3">
-            <div class="flex justify-between items-center">
-                <h4 class="text-muted">
-                    Send confirmation
-                </h4>
-                <Switch label="Send confirmation" />
-            </div>
-            <div class="flex justify-between items-center">
-                <h4 class="text-muted">
-                    Instant transfer
-                </h4>
-                <Switch label="Instant transfer" :isChecked="true" />
-            </div>
-        </div>
-        <div class="divider" />
-        <div class="section flex flex-col gap-3">
-            <div class="flex justify-between items-center">
-                <h4 class="text-muted">
-                    Amount
-                </h4>
-                <h3 class="price">
-                    $1000<span>.00</span>
-                </h3>
-            </div>
-            <div class="flex justify-between items-center">
-                <h4 class="text-muted">
-                    Transfer fee
-                </h4>
-                <h3 class="price">
-                    $10<span>.00</span>
-                </h3>
-            </div>
-            <div class="flex justify-between items-center">
-                <h4 class="text-muted">
-                    Network fee
-                </h4>
-                <div class="flex items-center gap-2">
-                    <div class="flex items-center px-2 rounded-lg bg-accent-faded text-accent">
-                        4%
-                    </div>
-                    <h3 class="price">
-                        $10<span>.00</span>
-                    </h3>
-                </div>
-            </div>
-        </div>
-        <div class="divider" />
-        <div class="section flex flex-col gap-3">
-            <div class="flex justify-between items-center">
-                <h4 class="font-bold">
-                    Total
-                </h4>
-                <h3 class="price">
-                    $1020<span>.00</span>
-                </h3>
-            </div>
-            <p class="text-muted mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum <span
-                    class="text-accent">terms & Conditions</span>
+  <Button
+    label="Open modal"
+    @click="openModal = true"
+  />
+  <Transition
+    name="modal"
+  >
+    <div
+      v-if="openModal"
+      class="container rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+    >
+      <div class="section flex justify-between items-center">
+        <Button
+          label="Cancel"
+          circle
+        />
+        <h2 class="text-muted text-lg">
+          Send payment
+        </h2>
+        <Button
+          label="Close"
+          circle
+          @click="openModal = false"
+        />
+      </div>
+      <div class="divider" />
+      <div class="section flex justify-between items-center">
+        <div class="flex items-center gap-4">
+          <img
+            src="https://avatars.githubusercontent.com/u/71938701?v=4"
+            alt="avatar"
+            class="size-14 rounded-full border-2 border-black"
+          >
+          <div>
+            <h4>
+              Hugo Richard
+            </h4>
+            <p class="text-muted">
+              contact@gmail.com
             </p>
+          </div>
         </div>
-        <div class="divider" />
-        <div class="section flex gap-3">
-            <Button label="Cancel" block />
-            <Button label="Send" primary block />
+        <Button
+          label="+"
+          circle
+        />
+      </div>
+      <div class="divider" />
+      <div class="section flex flex-col gap-3">
+        <div class="flex justify-between items-center">
+          <h4 class="text-muted">
+            Send confirmation
+          </h4>
+          <Switch label="Send confirmation" />
         </div>
+        <div class="flex justify-between items-center">
+          <h4 class="text-muted">
+            Instant transfer
+          </h4>
+          <Switch
+            label="Instant transfer"
+            :is-checked="true"
+          />
+        </div>
+      </div>
+      <div class="divider" />
+      <div class="section flex flex-col gap-3">
+        <div class="flex justify-between items-center">
+          <h4 class="text-muted">
+            Amount
+          </h4>
+          <h3 class="price">
+            $1000<span>.00</span>
+          </h3>
+        </div>
+        <div class="flex justify-between items-center">
+          <h4 class="text-muted">
+            Transfer fee
+          </h4>
+          <h3 class="price">
+            $10<span>.00</span>
+          </h3>
+        </div>
+        <div class="flex justify-between items-center">
+          <h4 class="text-muted">
+            Network fee
+          </h4>
+          <div class="flex items-center gap-2">
+            <div class="flex items-center px-2 rounded-lg bg-accent-faded text-accent">
+              4%
+            </div>
+            <h3 class="price">
+              $10<span>.00</span>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div class="divider" />
+      <div class="section flex flex-col gap-3">
+        <div class="flex justify-between items-center">
+          <h4 class="font-bold">
+            Total
+          </h4>
+          <h3 class="price">
+            $1020<span>.00</span>
+          </h3>
+        </div>
+        <p class="text-muted mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum <span
+            class="text-accent"
+          >terms & Conditions</span>
+        </p>
+      </div>
+      <div class="divider" />
+      <div class="section flex gap-3">
+        <Button
+          label="Cancel"
+          block
+        />
+        <Button
+          label="Send"
+          primary
+          block
+        />
+      </div>
     </div>
+  </Transition>
 </template>
 
 <style scoped>
@@ -120,5 +154,29 @@ const openModal = ref(false);
     span {
         @apply text-muted;
     }
+}
+
+.modal-enter-active {
+    @apply transition ease-in-out duration-300;
+}
+
+.modal-enter-from {
+    @apply opacity-0;
+}
+
+.modal-enter-to {
+    @apply opacity-100;
+}
+
+.modal-leave-active {
+    @apply transition ease-in-out duration-300;
+}
+
+.modal-leave-from {
+    @apply opacity-100;
+}
+
+.modal-leave-to {
+    @apply opacity-0;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['fastnuxt'],
+
   css: ['~/main.css'],
+
+  colorMode: {
+    preference: 'dark',
+    fallback: 'dark',
+    classSuffix: ''
+  },
+
   devtools: { enabled: true }
 })
